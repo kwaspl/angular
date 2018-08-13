b8e4deecb406d817cf34592fdb61f9c0de650c5f
szybipio <piotr.szybicki@brightone.pl>
2018-08-13 14:56:58 +0200

    initilal layout

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -2,12 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { HederComponent } from './heder/heder.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipiesListComponent } from './recipies/recipies-list/recipies-list.component';
+import { RecipieDetailComponent } from './recipies/recipie-detail/recipie-detail.component';
+import { RecipieItemComponent } from './recipies/recipies-list/recipie-item/recipie-item.component';
+import { ShoppigListComponent } from './shoppig-list/shoppig-list.component';
+import { ShoppingEditComponent } from './shoppig-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    HederComponent,
+    RecipiesComponent,
+    RecipiesListComponent,
+    RecipieDetailComponent,
+    RecipieItemComponent,
+    ShoppigListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
