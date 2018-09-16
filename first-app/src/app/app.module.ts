87973085eaf9aeda6e15ccdeb0dc89769b6514a6
piotr.szybicki piotr.szybicki <kwaspl>
2018-09-16 13:39:06 +0200

    first version of ngRx

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,27 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule }    from '@angular/common/http';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { CommunicationService } from './services/communication.service';
-import { UserInfoService } from './services/user-info.service'
 import { SmartComponent } from './smart/chat/smart.component';
-import { KeyExchangeService } from './services/key-exchange.service';
 import { PresnetationComponent } from './presentation/chat/presnetation.component'
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {  MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,
-  MatToolbarModule, MatMenuModule,MatIconModule, MatProgressSpinnerModule, MatDividerModule } from '@angular/material';
 import { PresentationLoginComponent } from './presentation/login/presentation-login/presentation-login.component';
 import { SmartLoginComponent } from './smart/login/smart-login/smart-login.component';
 import { SmartMenuBarComponent } from './smart/menu/smart-menu-bar/smart-menu-bar.component';
 import { PresentationMenuBarnComponent } from './presentation/menu/presentation-menu-barn/presentation-menu-barn.component';
 
+import { CommunicationService } from './services/communication.service';
+import { UserInfoService } from './services/user-info.service';
+import { KeyExchangeService } from './services/key-exchange.service';
 
-const routes: Routes = [
-    { path: 'chat', component: SmartComponent },
-    { path: 'login', component: SmartLoginComponent },
-    {path : '', component : SmartLoginComponent}
-  ];
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
+import { MaterialDesignModule } from './material-design.module';
+import { AppRoutingModule } from './router.module';
+import { AppComponentsModule } from './components.module';
 
 @NgModule({
   declarations: [
@@ -42,17 +43,10 @@ const routes: Routes = [
     HttpModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatDialogModule,
-    MatInputModule,
-    MatTableModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-    MatDividerModule,
-    RouterModule.forRoot(routes)
+    MaterialDesignModule,
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [CommunicationService,
               UserInfoService,
---
+++ first-app/src/app/router.module.ts
@@ -0,0 +1,21 @@
+import {NgModule}  from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SmartLoginComponent } from './smart/login/smart-login/smart-login.component';
+import { SmartComponent } from './smart/chat/smart.component';
+
+const routes: Routes = [
+    { path: 'chat', component: SmartComponent },
+    { path: 'login', component: SmartLoginComponent },
+    {path : '', component : SmartLoginComponent}
+  ];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
